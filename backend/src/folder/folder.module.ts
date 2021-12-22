52fdbf8f7826c3e8c1bff6c08c377893884c8fb7
ChachoPacho <bordonnet@hotmail.com>
2021-12-22 14:53:45 -0300

    Folders Service Created

--- backend/src/folder/folder.module.ts
+++ backend/src/folder/folder.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FolderController } from './controller/folder.controller';
+import { FolderService } from './service/folder.service';
 
 @Module({
-  controllers: [FolderController]
+  controllers: [FolderController],
+  providers: [FolderService]
 })
 export class FolderModule {}
