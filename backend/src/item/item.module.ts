6e6eded13adb4685588d673c56211243b0eea80f
ChachoPacho <bordonnet@hotmail.com>
2021-12-22 10:18:11 -0300

    Item Module created & configurated

--- backend/src/item/item.module.ts
+++ backend/src/item/item.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ItemService } from './service/item.service';
+import { ItemController } from './controller/item.controller';
+import { Item } from './entity/item.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
-export class ItemModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([Item])],
+  providers: [ItemService],
+  controllers: [ItemController]
+})
+export class ItemModule { }
