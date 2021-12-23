cab532c567724fd8477132d859d474fd968a3243
ChachoPacho <bordonnet@hotmail.com>
2021-12-23 15:46:21 -0300

    Feature: edit item; Added Mark/Unmark

--- frontend/src/components/ItemComponent.js
+++ frontend/src/components/ItemComponent.js
@@ -1,11 +1,15 @@
 import '../App.css';
 import { APIItems } from '../common/config';
 
-function ItemComponent(folderId, items, Updater) {
+function ItemComponent(items, Updater, EditItem) {
 
   const deleteItem = async (itemId) => {
-    await new APIItems(folderId).deleteItem(itemId);
+    await new APIItems(0).deleteItem(itemId);
     Updater(e => !e);
+  }
+
+  const handleMarkItem = async (itemId, btn) => {
+    new APIItems(0).modifyItem(itemId, { isMarked: btn.checked });
   }
 
   return (
@@ -13,9 +17,15 @@ function ItemComponent(folderId, items, Updater) {
       {
         items.map(item => (
           <div key={item.id} className="Element-item">
-            <div className="Item-title">- {item.title}</div>
+            <div className="Item-title">
+              <input
+                type="checkbox"
+                defaultChecked={item.isMarked}
+                onChange={(e) => handleMarkItem(item.id, e.target)}
+              /> {item.title}
+            </div>
             <div className="Item-link">
-              <a className="App-link" onClick={() => console.log(item)} >Edit</a>
+              <a className="App-link" onClick={() => EditItem(item)} >Edit</a>
               <a className="App-link" onClick={() => deleteItem(item.id)} >Remove</a>
             </div>
           </div>
