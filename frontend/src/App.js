fdedc5e292d822bed7b060fdf85dea81dc7c1b49
ChachoPacho <bordonnet@hotmail.com>
2021-12-23 11:28:34 -0300

    General View

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import FolderComponent from './components/FolderComponent';
+import { APIFolder } from './common/config';
 
 function App() {
+  const [folder, setFolder] = useState("");
+  const [elementTitle, setElementTitle] = useState("");
+  const [updateFolders, UpdateFolders] = useState(true);
+  const [updateItems, UpdateItems] = useState(true);
+
+  const addElement = async () => {
+    if (elementTitle) {
+      console.log(elementTitle, folder);
+
+      if (folder) {
+        UpdateItems(e => !e);
+
+        return
+      }
+
+      await APIFolder.createFolder(elementTitle);
+
+      setElementTitle("");
+      UpdateFolders(e => !e);
+    }
+  }
+
+  const writeTitle = (e) => setElementTitle(e.target.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="Folder-section">
+          <a className="Folder-link">Folders</a>
+          {(folder) ? " > " + folder : ""}
+        </div>
+        <div className="Element-container">
+          {
+            (folder)
+              ? folder
+              : FolderComponent(updateFolders, UpdateFolders)
+          }
+        </div>
+        <div className="Element-appender">
+          <input className="Appender-textInput" value={elementTitle} onChange={writeTitle} placeholder={"New " + (folder ? "Task" : "Folder")} />
+          <button className="Appender-button" onClick={addElement}>Add</button>
+        </div>
+      </div>
     </div>
   );
 }
