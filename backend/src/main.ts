c08aaad277e3125d6ea2ed4e1df35e12e4f56daf
ChachoPacho <bordonnet@hotmail.com>
2021-12-24 07:24:32 -0300

    Server return React App Bundle on Docker Image

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path/posix';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(4000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+
+  app.useStaticAssets(join(__dirname, 'client'));
+  app.setBaseViewsDir(join(__dirname, 'client'));
+
+  await app.listen(3000);
 }
 bootstrap();
